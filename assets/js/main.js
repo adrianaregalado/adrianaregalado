a54f591c23640e68c23cd480fd25802b3506c8a8
adrii4 <adri.regalado@live.com>
2019-09-14 21:35:52 -0500

    Fixed menu second dropdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,13 +7,28 @@
 /* TEST */
 
 //* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
-var dropdown = document.getElementsByClassName("dropdown");
+var dropdown = document.getElementsByClassName("about-me-dropdown");
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var dropdownContent = document.getElementsByClassName("dropdown-content")[0];
+    var dropdownContent = document.getElementsByClassName("about-me-dropdown-content")[0];
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+
+var dropdown = document.getElementsByClassName("professional-dropdown");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = document.getElementsByClassName("professional-dropdown-content")[0];
     if (dropdownContent.style.display === "block") {
       dropdownContent.style.display = "none";
     } else {
